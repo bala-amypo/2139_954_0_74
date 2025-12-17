4acf6a425e2019e5fc85d644ac786f7d3eb5a848
bala-amypo <bala@amypo.in>
2025-12-17 09:17:31 +0000

    User commit

--- src/main/java/com/example/demo/service/Studentserviceimpl.java
+++ src/main/java/com/example/demo/service/Studentserviceimpl.java
@@ -1,31 +1,31 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import java.util.*;
-import org.springframework.stereotype.Service;
+// import java.util.*;
+// import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.Studententity;
-import com.example.demo.service.Studentservice;
+// import com.example.demo.entity.Studententity;
+// import com.example.demo.service.Studentservice;
 
-@Service
-public class Studentserviceimpl implements Studentservice{
-    private final Map<Integer, Studententity>store=new HashMap<>();
-    private int counter =1;
+// @Service
+// public class Studentserviceimpl implements Studentservice{
+//     private final Map<Integer, Studententity>store=new HashMap<>();
+//     private int counter =1;
 
-    @Override
-    public Studententity saveData(Studententity st){
-        st.setId(counter++);
-        store.put(st.getId(),st);
-        return st;
-    }
+//     @Override
+//     public Studententity saveData(Studententity st){
+//         st.setId(counter++);
+//         store.put(st.getId(),st);
+//         return st;
+//     }
 
-    @Override
-    public List<Studententity>getAllStudent(){
-        return new ArrayList<>(store.values());
-    }
+//     @Override
+//     public List<Studententity>getAllStudent(){
+//         return new ArrayList<>(store.values());
+//     }
 
-    @Override
-    public Studententity getStudent(int id){
-        return store.get(id);
-    }
+//     @Override
+//     public Studententity getStudent(int id){
+//         return store.get(id);
+//     }
 
-}
+// }
