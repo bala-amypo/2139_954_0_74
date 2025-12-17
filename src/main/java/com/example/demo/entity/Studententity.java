5de0ac210b5a925ed8a63153d2bd1e6299242fee
bala-amypo <bala@amypo.in>
2025-12-17 09:47:30 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -1,35 +1,42 @@
 package com.example.demo.entity;
 
-public class Studententity{
-     private int id;
-     private String name;
-     private String email;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-     public Studententity() {}
-     
-     public Studententity(int id,String name,String email){
-      this.id=id;
-      this.name=name;
-      this.email=email;
-     }
+@Entity
+@Table(name = "student")
+public class Studententity {
 
-      public int getId(){
+    @Id
+    private int id;
+    private String name;
+    private int age;
+
+    public Studententity() {
+    }
+
+    public int getId() {
         return id;
-      }
-      public void setId(int id){
-        this.id=id;
-      }
-      public String getName(){
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
         return name;
-      }
-      public void setName(String name){
-        this.name=name;
-      }
-       public String getEmail(){
-        return email;
-      }
-      public void setEmail(String email){
-        this.email=email;
-      }
-     
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
