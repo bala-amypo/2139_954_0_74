cbb58632df015ee07c4f9f645bf179d39c822972
bala-amypo <bala@amypo.in>
2025-12-17 09:52:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/Studententity.java
+++ src/main/java/com/example/demo/entity/Studententity.java
@@ -2,20 +2,16 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "student")
 public class Studententity {
 
     @Id
     private int id;
     private String name;
-    private int age;
+    private String email;
 
-    public Studententity() {
-    }
-
+    // getters & setters
     public int getId() {
         return id;
     }
@@ -32,11 +28,11 @@ public class Studententity {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
+    public String getEmail() {
+        return email;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
